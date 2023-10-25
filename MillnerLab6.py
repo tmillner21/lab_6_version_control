dd43d41650c7f1ed2ca5197098621d90db2ae3f9
tmillner21 <tmillner@ufl.edu>
2023-10-25 09:20:16 -0400

    MillnerLab6.py

--- MillnerLab6.py
+++ MillnerLab6.py
@@ -3,9 +3,11 @@
 
 
 def main():
+    # Lists to help the encode/decode process.
     password_chars_original = []
     password_chars_encoded = []
 
+    # A while loop so the program runs until the user chooses to stop.
     while True:
 
         print("""Menu
@@ -15,20 +17,27 @@ def main():
 3. Quit""""")
         user_input = input("Please enter an option: ")
 
+        # If/else logic depending on the user's input.
         if user_input == "1":
             password_to_encode = input("Please enter your password to encode: ")
+
             for char in password_to_encode:
                 password_chars_original.append(int(char))
                 password_chars_encoded.append(int(char)+3)
             print("Your password has been encoded and stored!")
+
             for i in range(len(password_chars_encoded)):
                 password_chars_encoded[i] = str(password_chars_encoded[i])
+
         elif user_input == "2":
             # decode to be done by partner, if it's a two digit number, take the last digit.
             pass
+
+        # Exits program
         elif user_input == "3":
             exit()
 
 
+# Calls the main function.
 if __name__ == "__main__":
     main()
