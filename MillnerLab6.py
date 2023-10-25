3813f7eabc067a30e675997f1c08bb25b5b21867
anyawhitman1018 <annawhitman@ufl.edu>
2023-10-25 10:24:38 -0400

    MillnerLab6.py

--- MillnerLab6.py
+++ MillnerLab6.py
@@ -1,11 +1,38 @@
 # Tavienne Millner (encode function)
-# Partner (decode function)
+# Anya Whitman (decode function)
 
 
-def main():
-    # Lists to help the encode/decode process.
-    password_chars_original = []
+# Encode function.
+def encode(password_to_encode):
+    # List to help the encode process.
     password_chars_encoded = []
+
+    for char in password_to_encode:
+        password_chars_encoded.append(str(int(char) + 3))
+    print("Your password has been encoded and stored!")
+
+    return password_chars_encoded
+
+
+# Decode function.
+def decode(encoded_password):
+    password_chars_decoded = []
+
+    for char in encoded_password: #need to subract 3 from each encoded number
+        if char == "0": #the encoded number resulted in 10
+            password_chars_decoded.append("7")
+        elif char == "1": #the encoded number resulted in 11
+            password_chars_decoded.append("8")
+        elif char == "2": #the encoded number resulted in 12
+            password_chars_decoded.append("9")
+        else: #the decoded number is the encoded number - 3
+            password_chars_decoded.append(str(int(char)-3))
+
+    return password_chars_decoded
+
+
+# Main function.
+def main():
 
     # A while loop so the program runs until the user chooses to stop.
     while True:
@@ -19,19 +46,31 @@ def main():
 
         # If/else logic depending on the user's input.
         if user_input == "1":
+            # Gets user's password to encode.
             password_to_encode = input("Please enter your password to encode: ")
 
-            for char in password_to_encode:
-                password_chars_original.append(int(char))
-                password_chars_encoded.append(int(char)+3)
-            print("Your password has been encoded and stored!")
+            # List of encoded characters.
+            password_chars_encoded = encode(password_to_encode)
 
-            for i in range(len(password_chars_encoded)):
-                password_chars_encoded[i] = str(password_chars_encoded[i])
+            # Creating the encoded password string.
+            encoded_password = ""
+            for char in password_chars_encoded:
+                encoded_password += char[-1]
 
         elif user_input == "2":
-            # decode to be done by partner, if it's a two digit number, take the last digit.
-            pass
+            # Gets user's password to decode.
+            password_to_decode = input("please enter a password to decode: ")
+
+            # List of decoded characters.
+            password_chars_decoded = decode(password_to_decode)
+
+            # creating the decoded password string.
+            decoded_password = ""
+            for char in password_chars_decoded:
+                decoded_password += char
+
+            # Displaying the decoded password.
+            print(decoded_password)
 
         # Exits program
         elif user_input == "3":
